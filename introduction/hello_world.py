292bb22dac166b27fb96cd05f5f8ff9a1e504e8d
Bertrand LAFOUGERE <lafber@gmail.com>
2016-05-25 23:27:28 +0200

    chapitre 2 et 2.1

--- introduction/hello_world.py
+++ introduction/hello_world.py
@@ -1,1 +1,23 @@
-print("Hello World")
+# This is my first python script
+''' je vais faire deux lignes de commentaires
+parce que j'ai envie de parler'''
+
+phrase = "Hello, world"
+phrasebis = "Good morning"
+#print(phrase, phrasebis) # concatenation de deux variables
+
+""" pour un
+    résultat
+    impressionnant
+"""
+
+long_string_2_re_3 = """ ma super longue chaine sur
+plusieurs lignes
+déchires """
+print(long_string_2_re_3)
+
+long_string_2_re_4 = ''' et encore une chaine sur trois lignes \
+ la deuxième \
+ et la troisième'''
+
+print(long_string_2_re_4)
