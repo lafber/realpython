a6f819ac9455a6131874b0e705e9964618d95388
Bertrand LAFOUGERE <lafber@gmail.com>
2016-06-02 22:48:48 +0200

    chapitre 3

---
+++ 3.2_use_object_and_method.py
@@ -0,0 +1,9 @@
+''' Real Python
+    3.2 Use Objects and Methods
+'''
+
+response = input("Dis moi comment tu t'appelles : ")
+response = response.lower()
+
+print("Tu t'appelles réellement : ", response, '?')
+    
---
+++ 3.3_first_letter.py
@@ -0,0 +1,9 @@
+''' Real Python
+    3.3 Assignment: pick apart your user's input 
+'''
+
+input_password = input("Tell me your paswword please : ")
+
+first_letter = input_password[0:1].upper()
+
+print('Your password begins with', first_letter, 'and is', len(input_password), 'long')
