d4c107cd7bcd9ed39d48ba467f2c592997be0b83
Bertrand LAFOUGERE <lafber@gmail.com>
2016-08-31 22:08:01 +0200

    chapitre 9.1

--- chapitre_en_cours.py
+++ chapitre_en_cours.py
@@ -2,17 +2,11 @@
     Script Python générique pour exécuter les exemples du cours    
 '''
 
-my_input_file = open("hello.txt", "r")
-print("Line 0 (first line):", my_input_file.readline())
+import os
 
-my_input_file.seek(0) # jump back to beginning
-print("Line 0 again:", my_input_file.readline())
-print("Line 1:", my_input_file.readline())
+my_path = "/Users/bertrand/Documents/Projets/realpython"
+input_file_name = os.path.join(my_path, 'poem.txt')
 
-my_input_file.seek(8) # jump to character at index 8
-print("Line 0 (starting at 9th character):", my_input_file.readline())
-
-my_input_file.seek(10, 1) # relative jump forward 10 characters
-print("Line 1 (starting at 11th character):", my_input_file.readline())
-
-my_input_file.close()
+with open(input_file_name, 'r') as my_input_file:
+    for line in my_input_file.readlines():
+        print(line,)
